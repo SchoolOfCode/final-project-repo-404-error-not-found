b80bc380db41f7b20cbb68476aaed61b0078d974
D20CM <scottbrown500@hotmail.com>
2022-02-18 19:12:36 +0000

    add mentor photos

--- mentor-app/pages/allMentors/index.js
+++ mentor-app/pages/allMentors/index.js
@@ -1,5 +1,4 @@
 export const getStaticProps = async () => {
-
   const res = await fetch("http://localhost:3000/api/mentors");
   const data = await res.json();
 
@@ -11,21 +10,26 @@ export const getStaticProps = async () => {
 const allMentors = ({ mentors }) => {
   return (
     <div>
-
       <h1>Mentors</h1>
       {mentors.map((mentor) => {
         console.log({ mentors });
-        const { bio, firstName, email, socials } = mentor;
+        const { bio, firstname, email, socials, photourl, location } = mentor;
         return (
           <div key={mentor.id}>
             <a>
-              <h3>{firstName}</h3>
+              <h3>{firstname}</h3>
+              <h4>{location}</h4>
               <p>{email}</p>
               <p>{bio}</p>
-              {/* <p>{[0].socials[0]}</p> */}
+              <p>
+                {socials.name}: {socials.userName}
+              </p>
+              <img
+                src={photourl}
+                style={{ width: 100, height: 100, borderRadius: "50%" }}
+              ></img>
             </a>
           </div>
-
         );
       })}
     </div>
