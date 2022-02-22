cc445fe390178e19ef32fc76ce6596430eec2028
Chun Wai <lochunwai5@gmail.com>
2022-02-22 14:00:36 +0000

    login

--- mentor-app/pages/index.js
+++ mentor-app/pages/index.js
@@ -8,14 +8,12 @@ import Mentee from "../pages/sign-up/mentee";
 import Link from "next/link";
 
 export default function Home() {
-  
-
   const handleClick = (e, path) => {
-    if (path === "/sign-up/mentor") {
-      console.log("I clicked sign up mentor");
+    if (path === "/login/mentor") {
+      console.log("I clicked log in as mentor");
     }
-    if (path === "/sign-up/mentee") {
-      console.log("I clicked on the mentee sign up");
+    if (path === "/login/mentee") {
+      console.log("I clicked on the mentee login");
     }
   };
 
@@ -24,14 +22,20 @@ export default function Home() {
       <h1 className={styles.title}>Homepage</h1>
       <h2 className={styles.title}> Join as a...</h2>
       {/* <div className={styles.homeButtons}> */}
-      
-      <Link href="/sign-up/mentor">
-        <a onClick={(e) => handleClick(e, "/sign-up/mentor")}> <button className={styles.btn}>  Mentor  </button> </a>
-        </Link>
-  
-        <Link href="/sign-up/mentee">
-        <a onClick={(e) => handleClick(e, "/sign-up/mentor")}> <button className={styles.btn}>  Mentee </button> </a>
-        </Link>
-     </div>
+
+      <Link href="/login/mentor">
+        <a onClick={(e) => handleClick(e, "/login/mentor")}>
+          {" "}
+          <button className={styles.btn}> Mentor </button>{" "}
+        </a>
+      </Link>
+
+      <Link href="/login/mentee">
+        <a onClick={(e) => handleClick(e, "/login/mentee")}>
+          {" "}
+          <button className={styles.btn}> Mentee </button>{" "}
+        </a>
+      </Link>
+    </div>
   );
 }
