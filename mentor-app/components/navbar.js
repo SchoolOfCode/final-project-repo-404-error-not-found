ecfebba04e7325ddfdcce220a65e0f77b38c1e14
Chun Wai <lochunwai5@gmail.com>
2022-02-24 11:53:20 +0000

    branch home

--- mentor-app/components/navbar.js
+++ mentor-app/components/navbar.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import { Link } from "react-scroll";
+
 const Navbar = () => {
   return (
     <nav>
-
       <div className="logo">
         <h1>Mentor App</h1>
       </div>
       <Link href="/">
         <a>Home</a>
       </Link>
-      <Link href="/about">
+      <Link href="/#about">
         <a>About</a>
       </Link>
-      <Link href="/contact">
+      <Link href="/#contact">
         <a>Contact</a>
       </Link>
       <Link href="/allMentors">
@@ -21,7 +22,6 @@ const Navbar = () => {
       <Link href="/login">
         <a>Log In</a>
       </Link>
-
     </nav>
   );
 };
