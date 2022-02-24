fe00dfa0704866d7ce72f7e2beb01e2b0af52817
D20CM <scottbrown500@hotmail.com>
2022-02-24 14:48:40 +0000

    start style mentors list

--- mentor-app/pages/allMentors/index.js
+++ mentor-app/pages/allMentors/index.js
@@ -1,3 +1,5 @@
+import css from "./index.module.css";
+
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:3000/api/mentors");
   const data = await res.json();
@@ -24,22 +26,23 @@ const allMentors = ({ mentors }) => {
         } = mentor;
         return (
           <div key={userid}>
-            <a>
-              <h3>{firstname}</h3>
-              <h4>{location}</h4>
-              <p>{email}</p>
-              <p>{biography}</p>
-              <p>
-                {socials.name} {socials.userName}
-              </p>
-              {skills.map((skill, index) => (
-                <p key={index}>{skill}</p>
-              ))}
-
+            <a className={css.mentorCard}>
               <img
                 src={photourl}
                 style={{ width: 100, height: 100, borderRadius: "50%" }}
               ></img>
+              <div className={css.cardTextArea}>
+                <h3>{firstname}</h3>
+                <h4>{location}</h4>
+                <p>{email}</p>
+                <p>{biography}</p>
+                <p>
+                  {socials.name} {socials.userName}
+                </p>
+                {skills.map((skill, index) => (
+                  <p key={index}>{skill}</p>
+                ))}
+              </div>
             </a>
           </div>
         );
