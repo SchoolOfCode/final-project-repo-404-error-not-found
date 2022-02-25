49cfed082966b47b137f22d7561cca6d48c4249c
Simon Prato <simonprato11@gmail.com>
2022-02-25 12:28:36 +0000

    some changes

--- mentor-app/pages/profile/mentor.js
+++ mentor-app/pages/profile/mentor.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import { Row, Col } from "antd";
-import css from "./mentor.module.css";
+import React from 'react'
+import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
+import { Row, Col } from 'antd'
+import css from './mentor.module.css'
 
 export default function Profile() {
   return (
@@ -9,14 +9,18 @@ export default function Profile() {
       <Row>
         <Col span={2}>col-6</Col>
         <Col span={13}>col-6</Col>
-        <Col span={7}>
-          <div>
-            <div></div>
-            <div></div>
+        <Col span={7} className={css.sideDisplay}>
+          <div className={css.rightSquare}>
+            <div className={css.topSquare}>
+              <p>Description of what is offered</p>
+            </div>
+            <div className={css.lowSquare}>
+              <button>Edit Profile</button>
+            </div>
           </div>
         </Col>
         <Col span={2}>col-6</Col>
       </Row>
     </>
-  );
+  )
 }
