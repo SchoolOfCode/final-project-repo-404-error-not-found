263f0e7c5178521533a6b9b56a5e493fe8010853
Novel71 <NovelAlam@hotmail.com>
2022-02-28 16:40:22 +0000

    3 boxes

--- mentor-app/components/About.js
+++ mentor-app/components/About.js
@@ -1,13 +1,36 @@
+import styles from "../styles/Home.module.css"
+
 const About = () => {
   return (
     <div>
       <h1 className="#about">About</h1>
-      <h3></h3>
+      <p>
+      Whatever your background, experiences or skill set may be, Mentoree aims to provide a safe platform 
+      to connect people who are interested in tech or already in industry to share and exchange knowledge.
+      </p>
+      <h1>How does it work</h1>
+    
+      <div className= {styles.card}>
+  
+        <div className={styles.step1}>
+        {<img alt="example" style={{ width: 150 }} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        <h3> hello hi hey </h3>
+        <p> mentoree</p>
+        </div> 
 
-      <h3>How does it work?</h3>
-      <h3>What are the benefits of mentorship?</h3>
-    </div>
-  );
-};
+        <div className={styles.step2}>
+        {<img alt="example" style={{ width: 150 }} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        <h3> hello hi hey </h3>
+        <p> mentoree</p>
+        </div> 
 
+        <div className={styles.step3}>
+        {<img alt="example" style={{ width: 150 }} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        <h3> hello hi hey </h3>
+        <p> mentoree</p>
+        </div> 
+        </div>
+        </div>
+)}
+       ;
 export default About;
