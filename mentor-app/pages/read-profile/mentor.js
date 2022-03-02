e001548eb09c32b61b0adf3df770c7de50254c6d
Simon Prato <simonprato11@gmail.com>
2022-03-02 16:06:50 +0000

    allow access profile

--- mentor-app/pages/read-profile/mentor.js
+++ mentor-app/pages/read-profile/mentor.js
@@ -1,51 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import firebase from '../../firebase/clientApp'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
+import css from './mentor.module.css'
+import { server } from '../../config'
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import firebase from "../../firebase/clientApp";
-import { useAuthState } from "react-firebase-hooks/auth";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import css from "./mentor.module.css";
-import { server } from "../../config";
-
-
-import TwitterIcon from "../../components/TwitterIcon";
-import GithubIcon from "../../components/GithubIcon";
-import LinkedinIcon from "../../components/LinkedinIcon";
+import TwitterIcon from '../../components/TwitterIcon'
+import GithubIcon from '../../components/GithubIcon'
+import LinkedinIcon from '../../components/LinkedinIcon'
 export async function getServerSideProps(context) {
   return {
     props: {
       currentId: context.query.loginid,
     },
-  };
+  }
 }
 
-
 export default function Profile(props) {
-  const { currentId } = props;
-  console.log(currentId);
-  //currentMentor is the mentor pulled from our database
-  const [currentMentor, setCurrentMentor] = useState(null);
-  // user is the user provided by firebase
-  const [user, loading, error] = useAuthState(firebase.auth());
+  const { currentId } = props
+  const [currentMentor, setCurrentMentor] = useState(null)
+  const [user, loading, error] = useAuthState(firebase.auth())
 
-  //takes the firebase uid and fetches the corresponding mentor from database, then assigns it to currentMentor
   useEffect(async () => {
-    if (user !== null) {
+    const loginid = await currentId
+    console.log('about to send GET request!')
+    const res = await fetch(`${server}/api/mentors/${loginid}`)
+    const data = await res.json()
+    setCurrentMentor(data[0])
+  }, [])
 
-      const loginid = await currentId;
-      // const loginid = 'hJAvwClURqXX0aiqsKsIlXqNa0R2'
-      console.log("about to send GET request!");
-      const res = await fetch(`${server}/api/mentors/${loginid}`);
-
-      const data = await res.json();
-      setCurrentMentor(data[0]);
-    }
-  }, [user]);
-
-  //render page only if currentMentor is loaded, otherwise show loading text
   if (currentMentor !== null) {
-    // let socialsKey = Object.keys(currentMentor.socials);
-
     return (
       <div className={css.profileFullArea}>
         <h1>Mentor profile</h1>
@@ -57,15 +42,15 @@ export default function Profile(props) {
               src={currentMentor.photourl}
             ></img>
             <div className={css.socialsArea}>
-              {Object.keys(currentMentor.socials)[0] === "linkedin" ? (
+              {Object.keys(currentMentor.socials)[0] === 'linkedin' ? (
                 <LinkedinIcon
                   handle={Object.values(currentMentor.socials)[0]}
                 />
               ) : null}
-              {Object.keys(currentMentor.socials)[0] === "github" ? (
+              {Object.keys(currentMentor.socials)[0] === 'github' ? (
                 <GithubIcon handle={Object.values(currentMentor.socials)[0]} />
               ) : null}
-              {Object.keys(currentMentor.socials)[0] === "twitter" ? (
+              {Object.keys(currentMentor.socials)[0] === 'twitter' ? (
                 <TwitterIcon handle={Object.values(currentMentor.socials)[0]} />
               ) : null}
             </div>
@@ -106,12 +91,12 @@ export default function Profile(props) {
               <button>Send a Message</button>
               {/* </Link> */}
             </div>
-            <Link href="/allMentors">
+            <Link href='/allMentors'>
               <button>Back to Main</button>
             </Link>
           </div>
         </div>
       </div>
-    );
-  } else return <p>loading data...</p>;
+    )
+  } else return <p>loading data...</p>
 }
