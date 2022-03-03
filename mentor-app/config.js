efaeb0a2123e3f326b1cff68b2d3c12dd2f54198
D20CM <scottbrown500@hotmail.com>
2022-03-03 09:44:25 +0000

    update live project url in config

--- mentor-app/config.js
+++ mentor-app/config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://modest-mcnulty-376d20.netlify.app";
+  : "https://mentoree.netlify.app";
