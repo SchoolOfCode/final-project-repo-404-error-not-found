273a4a9cc1ddf5e8acc9ac8375219c04dfffa3e6
Simon Prato <simonprato11@gmail.com>
2022-03-07 10:01:41 +0000

    db added

---
+++ mentor-app/models/connection.js
@@ -0,0 +1,43 @@
+import query from '../database/index.js'
+
+//get all connections for mentor
+export async function getConnectionMentor(id) {
+  const response = await query(
+    'SELECT * FROM connection WHERE mentor_id = $1;',
+    [id]
+  )
+  return response.rows
+}
+//get all connections for mentee
+export async function getConnectionMentee(id) {
+  const response = await query(
+    'SELECT * FROM connection WHERE mentee_id = $1;',
+    [id]
+  )
+  return response.rows
+}
+// create connection from mentee side
+export async function createConnection(data) {
+  const { mentee_id, mentor_id } = data
+  const status = 'pending'
+  const res = await query(
+    'INSERT INTO connection (mentee_id, mentor_id, status) VALUES ($1, $2, $3)',
+    [mentee_id, mentor_id, status]
+  )
+  return res.rows
+}
+
+//delete connection
+export async function deleteConnection(id) {
+  let response = await query('DELETE FROM connection WHERE id = $1;', [id])
+  return response
+}
+
+export async function updateStatus(id) {
+  const status = 'accepted'
+  const response = await query(
+    `UPDATE connection SET status = ($1) WHERE id = ($2);`,
+    [status, id]
+  )
+  return response
+}
---
+++ mentor-app/pages/api/connection/[menteesid].js
@@ -0,0 +1,33 @@
+import { deleteMentee, updateMentee, getMentee } from '../../../models/mentee'
+
+export default async function (req, res) {
+  const HTTPMethod = req.method
+  let id = req.query.menteesid
+  let objectToUpdate = req.body
+
+  switch (HTTPMethod) {
+    case 'GET':
+      const mentee = await getMentee(id)
+      //call some imported function that handles logic/sql queries
+      res.status(200).json(mentee)
+      break
+    case 'PATCH':
+      id = String(id)
+      console.log('ID:' + id, ' Body: ' + objectToUpdate)
+
+      const editMentee = await updateMentee(id, objectToUpdate)
+      //call some imported function that handles logic/sql queries
+      res.status(200).json(editMentee)
+      break
+    case 'DELETE':
+      //call some imported function that handles logic/sql queries
+      // let id = req.params.mentorsid;
+      console.log(id)
+      const menteeToDelete = await deleteMentee(id)
+      res.status(200).json(menteeToDelete)
+      break
+    default:
+      res.setHeader('Allow', ['PATCH', 'DELETE', 'GET'])
+      res.status(405).end(`Method ${HTTPMethod} not Allowed`)
+  }
+}
