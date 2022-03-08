1393b59869339058b264da5df1f3a8760ff5699e
Chun Wai <lochunwai5@gmail.com>
2022-03-08 14:57:37 +0000

    mentee dashboard

--- mentor-app/pages/dashboard/mentee.js
+++ mentor-app/pages/dashboard/mentee.js
@@ -1,40 +1,103 @@
-import React from 'react'
-import { Row, Col } from 'antd'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import css from './dashboard.module.css'
-import CardDashboard from '../../components/CardDashboard'
-import Link from 'next/link'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import css from "./dashboard.module.css";
+import CardDashboard from "../../components/CardDashboard";
+import firebase from "../../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { server } from "../../config";
 
-export default function Mentor() {
+export default function Mentee() {
+  const [user, loading, error] = useAuthState(firebase.auth());
+  const [connectionAccepted, setConnectionAccepted] = useState(null);
+  const [connectionPending, setConnectionPending] = useState(null);
+  const [status, setStatus] = useState(false);
+
+  function filterData(data) {
+    const pendingData = data.filter((each) => {
+      return each.status === "pending";
+    });
+    if (pendingData.length !== 0) {
+      setConnectionPending(pendingData);
+    }
+    const acceptedData = data.filter((each) => {
+      return each.status === "accepted";
+    });
+    if (acceptedData.length !== 0) {
+      setConnectionAccepted(acceptedData);
+    }
+  }
+
+  useEffect(async () => {
+    if (user !== null) {
+      const loginid = user.uid;
+      const res = await fetch(`${server}/api/connection/${loginid}`);
+      const data = await res.json();
+      console.log(data);
+      filterData(data);
+    }
+  }, [user, status]);
+
+  // async function deleteRequest(id) {
+  //   const res = await fetch(`${server}/api/connectionMentor/${id}`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '',
+  //     },
+  //   })
+  //   const response = await res.json()
+  //   // if status is true, set status to false
+  //   setStatus(!status)
+  // }
+
   return (
     <div className={css.main}>
       <div span={18}>
         <h1 className={css.title}>Name's Dashboard</h1>
-        <Link href='/edit-profile/mentee'>
+        <Link href="/edit-profile/mentee">
           <Button
-            colorScheme='teal'
-            variant='outline'
-            style={{ margin: '10px 0' }}
+            colorScheme="teal"
+            variant="outline"
+            style={{ margin: "10px 0" }}
           >
             Edit Profile
           </Button>
         </Link>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: "center" }}>
           <h2 className={css.subtitle}>Your Mentor(s) are...</h2>
         </div>
         <div className={css.container}>
-          <CardDashboard />
-          <CardDashboard />
+          {connectionAccepted ? (
+            connectionAccepted.map((each, index) => {
+              return (
+                <CardDashboard info={each} key={index} roleUrl={"mentors"} />
+              );
+            })
+          ) : (
+            <div className={css.acceptedRequest}>
+              <p>You dont have any mentors yet! </p>
+            </div>
+          )}
         </div>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: "center" }}>
           <h2 className={css.subtitle}>Pending Applications</h2>
         </div>
         <div className={css.container}>
-          <CardDashboard />
-          <CardDashboard />
+          {connectionPending ? (
+            connectionPending.map((each, index) => {
+              return (
+                <CardDashboard info={each} key={index} roleUrl={"mentors"} />
+              );
+            })
+          ) : (
+            <div className={css.acceptedRequest}>
+              <p>You dont have any mentors yet! </p>
+            </div>
+          )}
         </div>
       </div>
       <div></div>
     </div>
-  )
+  );
 }
