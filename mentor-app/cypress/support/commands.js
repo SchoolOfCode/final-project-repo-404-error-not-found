9e64f7a09b956c6e5491ddf0618c6bb97acaacb4
D20CM <scottbrown500@hotmail.com>
2022-03-08 15:14:15 +0000

    test auth login flow with Cypress

--- mentor-app/cypress/support/commands.js
+++ mentor-app/cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import firebase from "../../firebase/clientApp";
+const firebaseConfig = {
+  apiKey: "AIzaSyDPuRlh4lUZE-e2qEG-GWflZblx56UNef0",
+  authDomain: "mentoree-1ab01.firebaseapp.com",
+  projectId: "mentoree-1ab01",
+  storageBucket: "mentoree-1ab01.appspot.com",
+  messagingSenderId: "35053699656",
+  appId: "1:35053699656:web:6c8b738652ff8c1ecf8e7d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// allow automatic login and logout for testing with custom commands cy.login and cy.logout
+Cypress.Commands.add(
+  "login",
+  (email = "bob@mentor.com", password = "123456") => {
+    return firebase.default.auth().signInWithEmailAndPassword(email, password);
+  }
+);
+
+Cypress.Commands.add("logout", () => {
+  return firebase.auth().signOut();
+});
