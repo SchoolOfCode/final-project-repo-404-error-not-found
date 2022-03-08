5bd95b66531e37ee31fd50d1561f0d6fc08f2135
Chun Wai <lochunwai5@gmail.com>
2022-03-08 12:14:28 +0000

    render cardDashboard

--- mentor-app/components/CardDashboard.js
+++ mentor-app/components/CardDashboard.js
@@ -1,20 +1,53 @@
-import React from 'react'
-import { Button } from '@chakra-ui/react'
-import styles from '../styles/CardDashboard.module.css'
-import css from '../pages/dashboard/dashboard.module.css'
+import React from "react";
+import { Button } from "@chakra-ui/react";
+import styles from "../styles/CardDashboard.module.css";
+import css from "../pages/dashboard/dashboard.module.css";
+import Link from "next/link";
+import { server } from "../config";
+import { useEffect, useState } from "react";
 
-export default function CardDashboard() {
+export default function CardDashboard(props) {
+  const { info } = props;
+  const [infoRender, setInfoRender] = useState(null);
+  useEffect(async () => {
+    if (info !== null) {
+      const loginid = info.mentee_id;
+      const res = await fetch(`${server}/api/mentees/${loginid}`);
+      const data = await res.json();
+      console.log(data);
+      setInfoRender(data);
+    }
+  }, [info]);
+
   return (
     <>
       <div className={css.card}>
-        <img
-          src='https://static.wikia.nocookie.net/disney/images/7/7b/Pluto.PNG/revision/latest/top-crop/width/360/height/360?cb=20170628205507'
-          alt=''
-          className={css.picture}
-        />
-        <h4 className={css.cardName}>Name Surname</h4>
-        <Button colorScheme='teal'>View Profile</Button>
+        {infoRender && (
+          <img src={infoRender[0].photourl} alt="" className={css.picture} />
+        )}
+
+        {infoRender ? (
+          <h4 className={css.cardName}>
+            {infoRender[0].firstname} {infoRender[0].surname}
+          </h4>
+        ) : (
+          "Name Surname"
+        )}
+        <Link
+          // when view profile button is clicked, line 41
+          href={{
+            pathname: "/read-profile/mentee",
+            // return the mentee information
+            query: {
+              loginid: info.mentee_id,
+            },
+          }}
+        >
+          <Button colorScheme="blue" variant="ghost">
+            View Profile
+          </Button>
+        </Link>
       </div>
     </>
-  )
+  );
 }
--- mentor-app/pages/dashboard/mentor.js
+++ mentor-app/pages/dashboard/mentor.js
@@ -1,86 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import css from './dashboard.module.css'
-import CardDashboard from '../../components/CardDashboard'
-import CardDashboardLong from '../../components/CardDasboardLong'
-import firebase from '../../firebase/clientApp'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { server } from '../../config'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import css from "./dashboard.module.css";
+import CardDashboard from "../../components/CardDashboard";
+import CardDashboardLong from "../../components/CardDasboardLong";
+import firebase from "../../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { server } from "../../config";
 
 export default function Mentor() {
-  const [user, loading, error] = useAuthState(firebase.auth())
-  const [connectionAccepted, setConnectionAccepted] = useState(null)
-  const [connectionPending, setConnectionPending] = useState(null)
+  const [user, loading, error] = useAuthState(firebase.auth());
+  const [connectionAccepted, setConnectionAccepted] = useState(null);
+  const [connectionPending, setConnectionPending] = useState(null);
 
   function filterData(data) {
     const pendingData = data.filter((each) => {
-      return each.status === 'pending'
-    })
+      return each.status === "pending";
+    });
     if (pendingData.length !== 0) {
-      setConnectionPending(pendingData)
+      setConnectionPending(pendingData);
     }
     const acceptedData = data.filter((each) => {
-      return each.status === 'accepted'
-    })
+      return each.status === "accepted";
+    });
     if (acceptedData.length !== 0) {
-      setConnectionAccepted(acceptedData)
+      setConnectionAccepted(acceptedData);
     }
   }
 
   useEffect(async () => {
     if (user !== null) {
-      const loginid = user.uid
-      const res = await fetch(`${server}/api/connectionMentor/${loginid}`)
-      const data = await res.json()
-      console.log(data)
-      filterData(data)
+      const loginid = user.uid;
+      const res = await fetch(`${server}/api/connectionMentor/${loginid}`);
+      const data = await res.json();
+      console.log(data);
+      filterData(data);
     }
-  }, [user])
+  }, [user]);
 
   async function acceptRequest(currentId) {
-    const data = { id: currentId }
+    const data = { id: currentId };
     const res = await fetch(`${server}/api/connection`, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '',
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "",
       },
       body: JSON.stringify(data),
-    })
-    const response = await res.json()
+    });
+    const response = await res.json();
   }
   async function deleteRequest(id) {
     const res = await fetch(`${server}/api/connectionMentor/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '',
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "",
       },
-    })
-    const response = await res.json()
+    });
+    const response = await res.json();
   }
 
   return (
     <div className={css.main}>
       <div span={18}>
         <h1 className={css.title}>Name's Dashboard</h1>
-        <Link href='/edit-profile/mentor'>
+        <Link href="/edit-profile/mentor">
           <Button
-            colorScheme='teal'
-            variant='outline'
-            style={{ margin: '10px 0' }}
+            colorScheme="teal"
+            variant="outline"
+            style={{ margin: "10px 0" }}
           >
             Edit Profile
           </Button>
         </Link>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: "center" }}>
           <h2 className={css.subtitle}>Your Mentees are...</h2>
         </div>
         <div className={css.container}>
           {/* if connectionAccepted is true render CardDashboard for each accepted connection, if not render <p>you dont have mentees</p> */}
           {connectionAccepted ? (
-            <CardDashboard />
+            connectionAccepted.map((each, index) => {
+              return <CardDashboard info={each} key={index} />;
+            })
           ) : (
             <div className={css.acceptedRequest}>
               <p>You dont have any mentee yet! </p>
@@ -88,7 +90,7 @@ export default function Mentor() {
           )}
         </div>
         <h2 className={css.subtitle}>
-          <div style={{ textAlign: 'center' }}>
+          <div style={{ textAlign: "center" }}>
             These mentees have requested mentorship from you...
           </div>
         </h2>
@@ -103,7 +105,7 @@ export default function Mentor() {
                   acceptRequest={acceptRequest}
                   deleteRequest={deleteRequest}
                 />
-              )
+              );
             })
           ) : (
             <div className={css.pendingRequest}>
@@ -114,5 +116,5 @@ export default function Mentor() {
       </div>
       <div></div>
     </div>
-  )
+  );
 }
