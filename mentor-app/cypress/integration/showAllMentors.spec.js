46bc80230836a454a21f05355453811ad0df1b52
D20CM <scottbrown500@hotmail.com>
2022-03-08 19:32:31 +0000

    test each mentor has a view profile button

--- mentor-app/cypress/integration/showAllMentors.spec.js
+++ mentor-app/cypress/integration/showAllMentors.spec.js
@@ -4,8 +4,8 @@ describe("All mentors page", () => {
   it("Should display a list of Mentors, each with a view profile button", () => {
     cy.visit(`${server}/allMentors`);
     cy.contains("Mentors").should("exist");
-    cy.get("[data-cy=mentorDisplayCard]").each(() => {
-      cy.contains("View Profile").should("exist");
+    cy.get("[data-cy=mentorDisplayCard]").each((item, index) => {
+      cy.wrap(item).should("contain.text", "View Profile");
     });
   });
 });
