0f37e1514347cc7bee7dd6b787dca36cd3fcf1e9
Chun Wai <lochunwai5@gmail.com>
2022-03-08 11:47:30 +0000

    mentee view profile

--- mentor-app/components/CardDasboardLong.js
+++ mentor-app/components/CardDasboardLong.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import { Button } from '@chakra-ui/react'
-import styles from '../styles/CardDashboard.module.css'
-import css from '../pages/dashboard/dashboard.module.css'
-import { server } from '../config'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { Button } from "@chakra-ui/react";
+import styles from "../styles/CardDashboard.module.css";
+import css from "../pages/dashboard/dashboard.module.css";
+import { server } from "../config";
+import { useEffect, useState } from "react";
+import { Link } from "next/link";
 
 export default function CardDashboardLong(props) {
-  const { info, acceptRequest, deleteRequest } = props
-  const [infoRender, setInfoRender] = useState(null)
+  const { info, acceptRequest, deleteRequest } = props;
+  const [infoRender, setInfoRender] = useState(null);
   useEffect(async () => {
     if (info !== null) {
-      const loginid = info.mentee_id
-      const res = await fetch(`${server}/api/mentees/${loginid}`)
-      const data = await res.json()
-      console.log(data)
-      setInfoRender(data)
+      const loginid = info.mentee_id;
+      const res = await fetch(`${server}/api/mentees/${loginid}`);
+      const data = await res.json();
+      console.log(data);
+      setInfoRender(data);
     }
-  }, [info])
+  }, [info]);
 
   return (
     <>
       <div className={css.card2}>
         {/* if infoRender is true render img, if not dont do anything */}
         {infoRender && (
-          <img src={infoRender[0].photourl} alt='' className={css.picture} />
+          <img src={infoRender[0].photourl} alt="" className={css.picture} />
         )}
         {/* if inforender is true render h4, if is not render line 33 'Name Surname'*/}
         {infoRender ? (
@@ -31,18 +32,28 @@ export default function CardDashboardLong(props) {
             {infoRender[0].firstname} {infoRender[0].surname}
           </h4>
         ) : (
-          'Name Surname'
+          "Name Surname"
         )}
-        <Button colorScheme='blue' variant='ghost'>
-          View Profile
-        </Button>
-        <Button colorScheme='teal' onClick={() => acceptRequest(info.id)}>
+        <Link
+          href={{
+            pathname: "/read-profile/mentee",
+            query: {
+              loginid: info.mentee_id,
+            },
+          }}
+        >
+          <Button colorScheme="blue" variant="ghost">
+            View Profile
+          </Button>
+        </Link>
+
+        <Button colorScheme="teal" onClick={() => acceptRequest(info.id)}>
           Accept
         </Button>
-        <Button colorScheme='red' onClick={() => deleteRequest(info.id)}>
+        <Button colorScheme="red" onClick={() => deleteRequest(info.id)}>
           Decline
         </Button>
       </div>
     </>
-  )
+  );
 }
