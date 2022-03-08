6370cda57dc39b2295179b9c9182a039c6f6a518
Chun Wai <lochunwai5@gmail.com>
2022-03-08 16:43:56 +0000

    remove email from all mentors page

--- mentor-app/pages/allMentors/index.js
+++ mentor-app/pages/allMentors/index.js
@@ -32,7 +32,7 @@ export const getServerSideProps = async () => {
 const AllMentors = ({ mentors }) => {
   return (
     <div>
-      <h1>Mentors</h1>
+      <h1 className={styles.allMentorsHeading}>Mentors</h1>
       {mentors.map((mentor) => {
         const {
           loginid,
@@ -63,7 +63,7 @@ const AllMentors = ({ mentors }) => {
               >
                 <Stack flex={1}>
                   <div className={styles.ImgContainer}>
-                    <Image boxSize="480px" src={photourl} alt="" />
+                    <Image boxSize="280px" src={photourl} alt="" />
                   </div>
                 </Stack>
                 <Stack
@@ -74,15 +74,17 @@ const AllMentors = ({ mentors }) => {
                   p={1}
                   pt={2}
                 >
-                  <Heading fontSize={"2xl"} fontFamily={"body"}>
-                    {firstname} {surname}
-                  </Heading>
-                  <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
-                    {jobtitle} at {company}
-                  </Text>
-                  <Text fontWeight={600} color={"gray.600"} size="sm" mb={4}>
+                  <HStack spacing="24px">
+                    <Heading fontSize={"3xl"} fontFamily={"body"}>
+                      {firstname} {surname}
+                    </Heading>
+                    <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
+                      {jobtitle} at {company}
+                    </Text>
+                  </HStack>
+                  {/* <Text fontWeight={600} color={"gray.600"} size="sm" mb={4}>
                     {email}
-                  </Text>
+                  </Text> */}
                   <Text fontWeight={600} color={"gray.500"} size="sm" mb={4}>
                     {location}
                   </Text>
