88f021631e5697cd64756ca91865ccece545cf7f
D20CM <scottbrown500@hotmail.com>
2022-03-09 09:41:17 +0000

    edit test to reflect removed h1 on all mentors page

--- mentor-app/cypress/integration/showAllMentors.spec.js
+++ mentor-app/cypress/integration/showAllMentors.spec.js
@@ -3,7 +3,7 @@ import { server } from "../../config";
 describe("All mentors page", () => {
   it("Should display a list of Mentors, each with a view profile button", () => {
     cy.visit(`${server}/allMentors`);
-    cy.contains("Mentors").should("exist");
+    // cy.contains("Mentors").should("exist");
     cy.get("[data-cy=mentorDisplayCard]").each((item, index) => {
       cy.wrap(item).should("contain.text", "View Profile");
     });
