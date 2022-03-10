01c1d473fc9277c4a67d87fef59ae80ebb49b5e5
Simon Prato <simonprato11@gmail.com>
2022-03-10 11:25:52 +0000

    db cleaning

--- mentor-app/database/scripts/resetMentorsTable.js
+++ mentor-app/database/scripts/resetMentorsTable.js
@@ -1,10 +1,8 @@
-import query from "../index.js";
+import query from '../index.js'
 
 async function deleteMentors() {
-  let res = await query(`DROP TABLE mentors`);
-  console.log("DROP mentors table", res);
-  let ressecond = await query(`DROP TABLE mentees`);
-  console.log("DROP mentors table", ressecond);
+  let res = await query(`DROP TABLE mentors`)
+  console.log('DROP mentors table', res)
 }
 
-deleteMentors();
+deleteMentors()
