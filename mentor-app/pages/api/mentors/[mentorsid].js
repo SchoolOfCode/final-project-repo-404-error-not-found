e9c089a32fb67f5f791af1384384f0547e45681e
D20CM <scottbrown500@hotmail.com>
2022-03-10 19:20:45 +0000

    more tests on mentors routes

--- mentor-app/pages/api/mentors/[mentorsid].js
+++ mentor-app/pages/api/mentors/[mentorsid].js
@@ -1,6 +1,6 @@
 import { deleteMentor, updateMentor, getMentor } from "../../../models/mentors";
 
-export default async function (req, res) {
+export default async function handler(req, res) {
   const HTTPMethod = req.method;
   let id = req.query.mentorsid;
   let objectToUpdate = req.body;
@@ -10,24 +10,22 @@ export default async function (req, res) {
   switch (HTTPMethod) {
     case "GET":
       const mentor = await getMentor(id);
-      //call some imported function that handles logic/sql queries
       res.status(200).json(mentor);
       break;
+
     case "PATCH":
       id = String(id);
       console.log("ID:" + id, " Body: " + objectToUpdate);
-
       const editMentor = await updateMentor(id, objectToUpdate);
-      //call some imported function that handles logic/sql queries
       res.status(200).json(editMentor);
       break;
+
     case "DELETE":
-      //call some imported function that handles logic/sql queries
-      // let id = req.params.mentorsid;
       console.log(id);
       const mentorToDelete = await deleteMentor(id);
       res.status(200).json(mentorToDelete);
       break;
+
     default:
       res.setHeader("Allow", ["PATCH", "DELETE", "GET"]);
       res.status(405).end(`Method ${HTTPMethod} not Allowed`);
