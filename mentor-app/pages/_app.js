e921f2580810704b49a02b5489eeaa75432d3161
D20CM <scottbrown500@hotmail.com>
2022-03-18 19:42:15 +0000

    add favicon

--- mentor-app/pages/_app.js
+++ mentor-app/pages/_app.js
@@ -1,12 +1,12 @@
-import Layout from '../components/layout'
-import '../styles/globals.css'
-import '../styles/AllMentors.module.css'
-import '../styles/Home.module.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import App from '.'
-import { AnimatePresence } from 'framer-motion'
-import Title from 'antd/lib/skeleton/Title'
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import "../styles/AllMentors.module.css";
+import "../styles/Home.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from ".";
+import { AnimatePresence } from "framer-motion";
+import Title from "antd/lib/skeleton/Title";
 function MyApp({ Component, pageProps }) {
   return (
     <AnimatePresence exitBeforeEnter>
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         </Layout>
       </ChakraProvider>
     </AnimatePresence>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
